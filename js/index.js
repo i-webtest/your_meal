d9507b850bd2233ea9b7268a20356c8b75c17486
i-webtest <stasbass80@gmail.com>
2022-11-30 16:45:11 +0300

    burgerMax

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const modalProduct = document.querySelector(".modal_product");
 const catalogtList = document.querySelector(".catalog__list");
 
-const product = {
+const burgerMax = {
   title: "Бургер Макс",
   price: 10000,
   weight: 5000,
@@ -11,36 +11,40 @@ const product = {
   ingredients: ["Пшеничная булочка", "Мега котлета из говядины", "Много сыра", "Листья салата", "Чипотл"],
 };
 
-const modalProductTitle = document.querySelector(".modal-product__title");
-const modalProductImage = document.querySelector(".modal-product__image");
-const modalProductDescription = document.querySelector(".modal-product__description");
-const ingredientsList = document.querySelector(".ingredients__list");
-const ingredientsCalories = document.querySelector(".ingredients__calories");
-const modalProductPriceCount = document.querySelector(".modal-product__price-count");
+const openModal = (product) => {
+  const modalProductTitle = document.querySelector(".modal-product__title");
+  const modalProductImage = document.querySelector(".modal-product__image");
+  const modalProductDescription = document.querySelector(".modal-product__description");
+  const ingredientsList = document.querySelector(".ingredients__list");
+  const ingredientsCalories = document.querySelector(".ingredients__calories");
+  const modalProductPriceCount = document.querySelector(".modal-product__price-count");
 
-modalProductTitle.textContent = product.title;
-modalProductImage.src = product.image;
-ingredientsList.textContent = "";
+  modalProductTitle.textContent = product.title;
+  modalProductImage.src = product.image;
+  ingredientsList.textContent = "";
 
-modalProductDescription.textContent = product.description;
-ingredientsCalories.textContent = `${product.weight}г, ккал ${product.calories}`;
-modalProductPriceCount.textContent = product.price;
+  modalProductDescription.textContent = product.description;
+  ingredientsCalories.textContent = `${product.weight}г, ккал ${product.calories}`;
+  modalProductPriceCount.textContent = product.price;
 
-const ingredientsListItems = product.ingredients.map((item) => {
-  const li = document.createElement("li");
+  const ingredientsListItems = product.ingredients.map((item) => {
+    const li = document.createElement("li");
 
-  li.classList.add("ingredients__item");
-  li.textContent = item;
-  return li;
-});
+    li.classList.add("ingredients__item");
+    li.textContent = item;
+    return li;
+  });
 
-ingredientsList.append(...ingredientsListItems);
+  ingredientsList.append(...ingredientsListItems);
+
+  modalProduct.classList.add("modal_open");
+};
 
 catalogtList.addEventListener("click", (event) => {
   const target = event.target;
 
   if (target.closest(".product__detail") || target.closest(".product__image")) {
-    modalProduct.classList.add("modal_open");
+    openModal(burgerMax);
   }
 });
 
